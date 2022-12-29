8780dbf47897980d7b49adc6aab9e1349c86eeb6
Lorenzo Cavuoti <lorenzocav97@gmail.com>
2022-12-29 17:20:03 +0100

    made briscola AECEnv compatible

--- src/env/briscola/utils.py
+++
@@ -1,9 +0,0 @@
-from src.env.briscola.BriscolaConstants import Constants
-
-
-def get_seed(card: int) -> int:
-    return card // Constants.cards_per_seed
-
-
-def get_rank(card: int) -> int:
-    return card % Constants.cards_per_seed
